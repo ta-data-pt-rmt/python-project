1f7c1d836f70f486323442f6124f0f1e07ebb36d
MiguelMendesDA <miguelmendesdataanalyst@gmail.com>
2023-10-17 10:16:11 +0100

    Update

--- your-code/TheraPac.py
+++ your-code/TheraPac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 # define rooms and items
@@ -17,37 +17,109 @@ door_a = {
     "type": "door",
 }
 
+door_b = {
+    "name": "door b",
+    "type": "door",
+}
+
+door_c = {
+    "name": "door c",
+    "type": "door",
+}
+
+door_d= {
+    "name": "door d",
+    "type": "door",
+}
 key_a = {
     "name": "key for door a",
     "type": "key",
     "target": door_a,
 }
 
+key_b = {
+    "name": "key for door b",
+    "type": "key",
+    "target": door_b,
+}
+
+key_c = {
+    "name": "key for door c",
+    "type": "key",
+    "target": door_c,
+}
+key_d = {
+    "name": "key for door d",
+    "type": "key",
+    "target": door_d,
+}
 piano = {
     "name": "piano",
     "type": "furniture",
 }
 
+queen_bed = {
+    "name": "queen bed",
+    "type": "furniture",
+}
+
+double_bed = {
+    "name": "double bed",
+    "type": "furniture",
+}
+
+dresser = {
+    "name": "dresser",
+    "type": "furniture",
+}
+
+dinning_table = {
+    "name": "dinning table",
+    "type": "furniture",
+}
 game_room = {
     "name": "game room",
     "type": "room",
+}
+
+bedroom_1 = {
+    "name": "bedroom 1",
+    "type": "room"
+}
+
+bedroom_2 = {
+    "name": "bedroom 2",
+    "type": "room"
+}
+living_room = {
+    "name": "living room",
+    "type": "room"
 }
 
 outside = {
   "name": "outside"
 }
 
-all_rooms = [game_room, outside]
+all_rooms = [game_room, bedroom_1, bedroom_2,living_room, outside]
 
-all_doors = [door_a]
+all_doors = [door_a, door_b, door_c, door_d]
 
 # define which items/rooms are related
 
 object_relations = {
     "game room": [couch, piano, door_a],
+    "bedroom 1": [door_a, door_b, door_c, queen_bed],
     "piano": [key_a],
-    "outside": [door_a],
-    "door a": [game_room, outside]
+    "queen bed": [key_b],
+    "double bed": [key_c],
+    "dresser": [key_d],
+    "outside": [door_d],
+    "door a": [game_room, bedroom_1],
+    "door b": [bedroom_1, bedroom_2],
+    "door c": [bedroom_1, living_room],
+    "door d": [living_room, outside],
+    "bedroom 2": [dresser, double_bed,door_b],
+    "living room": [door_d, dinning_table , door_c]
 }
 
 # define game state. Do not directly change this dict. 
@@ -62,7 +134,7 @@ INIT_GAME_STATE = {
 }
 
 
-# In[2]:
+# In[ ]:
 
 
 def linebreak():
@@ -71,7 +143,6 @@ def linebreak():
     """
     print("\n\n")
     
-
 def video ():
     video_path = "Presentation_Video.mp4"  
     cap = cv2.VideoCapture(video_path)
@@ -89,6 +160,7 @@ def video ():
     cap.release()
     cv2.destroyAllWindows()
 
+
 def start_game():
     """
     Start the game
@@ -105,17 +177,17 @@ def play_room(room):
     game_state["current_room"] = room
     if(game_state["current_room"] == game_state["target_room"]):
         print("Congrats! You escaped the room!")
-        exit()
     else:
         print("You are now in " + room["name"])
-        intended_action = input("What would you like to do? Type 'explore' or 'examine'?").strip()
+        intended_action = input("What would you like to do? Type 'explore' or 'examine' followed by item name?").strip()
         if intended_action == "explore":
             explore_room(room)
             play_room(room)
-        elif intended_action == "examine":
-            examine_item(input("What would you like to examine?").strip())
+        # UPDATE: merging the examine from two steps into single input to make it easier
+        elif intended_action.find("examine ")== 0:
+            examine_item(intended_action[8:].strip())
         else:
-            print("Not sure what you mean. Type 'explore' or 'examine'.")
+            print("Not sure what you mean. Type 'explore' or 'examine' followed by item name.")
             play_room(room)
         linebreak()
 
@@ -123,6 +195,10 @@ def explore_room(room):
     """
     Explore a room. List all items belonging to this room.
     """
+    # UPDATE: Checking to ensure object exists
+    if room["name"] not in object_relations:
+        print("Something wrong with the game settings! Do something!")
+    
     items = [i["name"] for i in object_relations[room["name"]]]
     print("You explore the room. This is " + room["name"] + ". You find " + ", ".join(items))
 
@@ -131,6 +207,12 @@ def get_next_room_of_door(door, current_room):
     From object_relations, find the two rooms connected to the given door.
     Return the room that is not the current_room.
     """
+    
+    # UPDATE: Checking to ensure object exists
+    if door["name"] not in object_relations:
+        # returning empty string to mark it as not found
+        return ""
+    
     connected_rooms = object_relations[door["name"]]
     for room in connected_rooms:
         if(not current_room == room):
@@ -147,9 +229,17 @@ def examine_item(item_name):
     play either the current or the next room depending on the game state
     to keep playing.
     """
+    
+    
     current_room = game_state["current_room"]
     next_room = ""
     output = None
+    
+    #UPDATE: checking if room exists in the object relations
+    if current_room["name"] not in object_relations:
+        print("Game settings are wrong! Aborting")
+        linebreak()
+        return
     
     for item in object_relations[current_room["name"]]:
         if(item["name"] == item_name):
@@ -191,6 +281,25 @@ game_state = INIT_GAME_STATE.copy()
 video()
 
 start_game()
+
+"""
+IMPROVEMENTS
+============
+
+1. Merged the two steps needed for the 'examine' step, to be in single command,
+   so instead of writing 'examine' first, and then in another input to type
+   'piano', you can now type 'examine piano' which is easier for the player.
+
+2. Added error checking to ensure object definitions are correct and the game
+   will not crash because of bad game settings, like forgetting to add items 
+   to object_relations, etc.
+"""
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
